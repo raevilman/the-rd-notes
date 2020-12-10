6df12d8f0d40d1c3521056d135f89b5138ef563f
raevilman <8374679+raevilman@users.noreply.github.com>
2020-12-10 13:34:13 +0530

    🐦 Adds twitter link

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import NotesLayout from '../components/NotesLayout';
+import React from "react";
+import NotesLayout from "../components/NotesLayout";
 
 export default ({ data }) => {
-    const { frontmatter, html } = data.markdownRemark;
-    return (
+  const { frontmatter, html } = data.markdownRemark;
+  return (
+    <>
       <NotesLayout frontmatter={frontmatter}>
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </NotesLayout>
-    );
-  };
+      <div className="flex justify-center gap-1 m-2 mb-4 ">
+        <span className="text-gray-700">lets connect via</span>
+        <a href="https://twitter.com/raevilman" target="_blank" class="">
+          <span className="text-gray-800">twitter</span>
+        </a>
+      </div>
+    </>
+  );
+};
 
 export const query = graphql`
-query PostsBySlug($slug: String!) {
+  query PostsBySlug($slug: String!) {
     markdownRemark(frontmatter: { slug: { eq: $slug } }) {
       html
       frontmatter {
