a10ba2c7fbd29e1ffa3f5cf2f88d2abc5eda45f2
Raman Dhawan <8374679+raevilman@users.noreply.github.com>
2021-02-08 20:19:12 +0530

    Adds counters by eventscounter.com

--- src/components/NotesLayout.js
+++ src/components/NotesLayout.js
@@ -1,24 +1,102 @@
 import React from "react";
 import Content from "../components/Content";
 import Layout from "../components/Layout";
+import { EventsCounter, IconEmoji } from "react-events-counter";
 
 function NotesLayout({ children, frontmatter, location }) {
   return (
     <>
       <Layout title={frontmatter.title}>
         <div className="flex justify-between text-invert">
-          <div>
+          <div className="w-full flex justify-between">
             <h1>{frontmatter.title}</h1>
+            <EventsCounter
+              asset={frontmatter.title}
+              event="page-views"
+              application="theRDnotes"
+              text="👁 &nbsp;"
+              countOnLoad={true}
+              className="flex "
+            />
           </div>
         </div>
         <Content>{children}</Content>
         <Content>
           <h3>Helpful?</h3>
           <p>
-          If you think this is helpful 🎈<br/>
-          Don't keep it to yourself 🙊 <br/><br/>
-          Share it with your <em>lovely</em> followers at <a href={'https://twitter.com/intent/tweet?text=Sharing an article I read, \''+frontmatter.title+'\' by @raevilman &url='+location.href} target="_blank"><span className="italic">twitter</span></a> 🗽 
+            If you think this is helpful 🎈
+            <br />
+            Don't keep it to yourself 🙊 <br />
+            <br />
+            Share it with your <em>lovely</em> followers at{" "}
+            <a
+              href={
+                "https://twitter.com/intent/tweet?text=Sharing an article I read, '" +
+                frontmatter.title +
+                "' by @raevilman &url=" +
+                location.href
+              }
+              target="_blank"
+            >
+              <span className="italic">twitter</span>
+            </a>{" "}
+            🗽
           </p>
+        </Content>
+        <Content>
+          <h3>Feedback!</h3>
+          <div className="">
+            <EventsCounter
+              asset={frontmatter.title}
+              event="page-likes"
+              application="theRDnotes"
+              text=" YES - I liked it &nbsp;"
+              formatCount="({count})"
+              icon={
+                <IconEmoji
+                  symbol="👍&nbsp;"
+                  label="views"
+                  className="cursor-pointer"
+                />
+              }
+              className="flex "
+            />
+            <EventsCounter
+              asset={frontmatter.title}
+              event="page-mehs"
+              application="theRDnotes"
+              text=" MEH &nbsp;"
+              formatCount="({count})"
+              icon={
+                <IconEmoji
+                  symbol="😐&nbsp;"
+                  label="views"
+                  className="cursor-pointer"
+                />
+              }
+              className="flex "
+            />
+            <EventsCounter
+              asset={frontmatter.title}
+              event="page-dislikes"
+              application="theRDnotes"
+              text=" NOPE - I hate it &nbsp;"
+              formatCount="({count})"
+              icon={
+                <IconEmoji
+                  symbol="👎&nbsp;"
+                  label="views"
+                  className="cursor-pointer"
+                />
+              }
+              className="flex "
+            />
+            <div className="flex justify-end">
+              <span className="text-xs text-gray-600">
+              powered by <a href="https://eventscounter.com" target="__blank">EventsCounter.com</a>
+              </span>
+            </div>
+          </div>
         </Content>
       </Layout>
     </>
