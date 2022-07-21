d394244e5567a74bfdc9ad28bece3b3fcae0b97b
raevilman <8374679+raevilman@users.noreply.github.com>
2022-07-21 20:00:32 +0530

    add recent posts section on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,66 @@ export default ({ data }) => {
             </div>
           </div>
         </div>
+        <section>
+          <h3> Recent Posts!</h3>
+          <div className="px-4 py-2">
+            {data.allMarkdownRemark.edges.map((post) => {
+              const {
+                title,
+                description,
+                slug,
+                author,
+                date_modified,
+                is_project,
+              } = post.node.frontmatter;
+              const excerpt = post.node.excerpt;
+              return (
+                !is_project && (
+                  <>
+                    <p className="text-gray-600 text-xs">{date_modified}</p>
+                    <Link to={slug}>{title}</Link>
+                    <p className="text-gray-800">{description}</p>
+                    <br />
+                  </>
+                )
+              );
+            })}
+          </div>
+        </section>
       </Layout>
     </div>
   );
 };
+
+
+export const query = graphql`
+  query SiteIndexQuery {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date_modified], order: DESC}
+      filter: {
+        frontmatter: {
+          is_published: { eq: true }
+          show_in_recent: { eq: true }
+        }
+      }
+    ) {
+      edges {
+        node {
+          id
+          excerpt
+          frontmatter {
+            title
+            description
+            slug
+            date_modified
+            author
+            is_published
+            show_in_recent
+            is_project
+          }
+        }
+      }
+    }
+  }
+`;
+
