0fa1f151331fdc11e3283a16d8150e910590f319
raevilman <8374679+raevilman@users.noreply.github.com>
2022-10-01 13:30:38 +0530

    add projects to recent list

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,33 +9,37 @@ export default ({ data }) => {
   return (
     <div>
       <Layout title="theRDnotes">
-        <div className="p-16 flex justify-center items-center bg-gray-100" style={{
-minHeight: `50vh`,
-borderRadius: `10px`,
-margin: `0.5rem`,}}>
+        <div
+          className="p-16 flex justify-center items-center bg-gray-100"
+          style={{
+            minHeight: `50vh`,
+            borderRadius: `10px`,
+            margin: `0.5rem`,
+          }}
+        >
           <div className="flex flex-col gap-8">
             <section className="border-l-2 border-orange-300 pl-2">
               <FaRegFileAlt />
               <Link to="/explorer">Notes Explorer</Link>
             </section>
             <div className="flex gap-6">
-            <section className="border-l-2 border-gray-700 pl-2">
-              <FaGithub />
-              <a href="https://github.com/raevilman" target="_blank">
-                GitHub
-              </a>
-            </section>
-            <section className="border-l-2 border-blue-400 pl-2">
-              <FaTwitter />
-              <a href="https://twitter.com/raevilman" target="_blank">
-                Twitter
-              </a>
-            </section>
+              <section className="border-l-2 border-gray-700 pl-2">
+                <FaGithub />
+                <a href="https://github.com/raevilman" target="_blank">
+                  GitHub
+                </a>
+              </section>
+              <section className="border-l-2 border-blue-400 pl-2">
+                <FaTwitter />
+                <a href="https://twitter.com/raevilman" target="_blank">
+                  Twitter
+                </a>
+              </section>
             </div>
           </div>
         </div>
         <section>
-          <h3> Recent Posts!</h3>
+          <h3> Recent posts!</h3>
           <div className="px-4 py-2">
             {data.allMarkdownRemark.edges.map((post) => {
               const {
@@ -48,14 +52,16 @@ margin: `0.5rem`,}}>
               } = post.node.frontmatter;
               const excerpt = post.node.excerpt;
               return (
-                !is_project && (
-                  <>
-                    <p className="text-gray-600 text-xs">{date_modified}</p>
-                    <Link to={slug}>{title}</Link>
-                    <p className="text-gray-800">{description}</p>
-                    <br />
-                  </>
-                )
+                <>
+                  <p className="text-gray-600 text-xs">{date_modified}
+                  
+                  { is_project && <span className="text-gray-700 text-xs ml-4">[Project]</span>}
+                  </p> 
+                  <Link to={slug}>{title}</Link> 
+                  <p className="text-gray-800">{description}</p>
+                  
+                  <br />
+                </>
               );
             })}
           </div>
@@ -65,11 +71,10 @@ margin: `0.5rem`,}}>
   );
 };
 
-
 export const query = graphql`
   query SiteIndexQuery {
     allMarkdownRemark(
-      sort: { fields: [frontmatter___date_modified], order: DESC}
+      sort: { fields: [frontmatter___date_modified], order: DESC }
       filter: {
         frontmatter: {
           is_published: { eq: true }
@@ -96,4 +101,3 @@ export const query = graphql`
     }
   }
 `;
-
